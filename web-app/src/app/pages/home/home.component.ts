079a3180363c37d8b301a85a21e34ed5717df107
John Christiansen <johnchristiansen@gmail.com>
2025-09-07 17:07:58 -0600

    (fix): clear state after page back button following taking a quiz

--- web-app/src/app/pages/home/home.component.ts
+++ web-app/src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { QuizGeneratorComponent } from '../../components/quiz-generator/quiz-generator.component';
 import { QuizDisplayComponent } from '../../components/quiz-display/quiz-display.component';
 import { QuizResultsComponent } from '../../components/quiz-results/quiz-results.component';
+import { clearAllState } from '../../store/quiz.actions';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,11 @@ import { QuizResultsComponent } from '../../components/quiz-results/quiz-results
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    // Clear all quiz state when navigating to home page
+    this.store.dispatch(clearAllState());
+  }
+}
